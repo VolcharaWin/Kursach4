6d9b93cabf5b04a02da31c543caf95ca43a31c31
VOLCHARIK PC <boysepbin@mail.ru>
2024-11-04 01:42:46 +0300

    Made separate login and main windows. Need to make display of all the tables in the database and then finally make the menu options and windows

---
+++ gostinitsa/login_window.py
@@ -0,0 +1,79 @@
+import sys, psycopg2
+from PyQt6.QtCore import Qt, pyqtSignal
+from PyQt6.QtWidgets import QWidget, QLabel, QApplication, QPushButton, QWidget, QLineEdit, QVBoxLayout, QMessageBox, QGridLayout
+
+DATABASE = "Gostinitsa"
+USER = '1'
+HOST = "localhost"
+PORT = "5432"
+class LogInWindow(QWidget):
+    connection_successful = pyqtSignal(object)
+    def __init__(self):
+        super().__init__()
+        central_widget = QWidget(self)
+        self.setWindowTitle('Подключение')
+
+        db_name_label = QLabel(f'Подключение к базе данных {DATABASE}')
+
+        btn_connect = QPushButton('Подключиться')
+        btn_connect.setAutoDefault(True)
+
+        user_label = QLabel()
+        user_label.setText('Пользователь: ')
+
+        password_label = QLabel()
+        password_label.setText('Пароль: ')
+
+        self.user_line = QLineEdit()
+        self.user_line.returnPressed.connect(btn_connect.click)
+        self.password_line = QLineEdit()
+        self.password_line.returnPressed.connect(btn_connect.click)
+        self.password_line.setEchoMode(QLineEdit.EchoMode.Password)
+
+        form_layout = QGridLayout()
+        form_layout.addWidget(user_label, 0, 0)
+        form_layout.setAlignment(user_label, Qt.AlignmentFlag.AlignRight)
+
+        form_layout.addWidget(self.user_line, 0, 1)
+        form_layout.setAlignment(self.user_line, Qt.AlignmentFlag.AlignLeft)
+
+        form_layout.addWidget(password_label, 1, 0)
+        form_layout.setAlignment(password_label, Qt.AlignmentFlag.AlignRight)
+
+        form_layout.addWidget(self.password_line, 1, 1)
+        form_layout.setAlignment(self.password_line, Qt.AlignmentFlag.AlignLeft)
+
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(db_name_label, alignment=Qt.AlignmentFlag.AlignCenter)
+        main_layout.addLayout(form_layout)
+        main_layout.addWidget(btn_connect, alignment=Qt.AlignmentFlag.AlignCenter)
+
+        self.setLayout(main_layout)
+
+        self.setFixedSize(300, 200)
+        btn_connect.clicked.connect(self.connect_to_database)
+    def connect_to_database(self):
+        user = self.user_line.text()
+        password = self.password_line.text()
+
+        if not all([user, password]):
+            QMessageBox.critical(self, 'Ошибка', 'Заполните все поля')
+            return None
+        try:
+            connection = psycopg2.connect(
+                database=DATABASE,
+                user=user,
+                password=password,
+                host=HOST,
+                port=PORT
+            )
+            QMessageBox.information(self, 'Успех', f'Успешное подключение к базе данных')
+            print("Успешное подключение к базе данных")
+
+            self.connection_successful.emit(connection)
+            self.close()
+            return connection
+        except (Exception, psycopg2.Error) as error:
+            QMessageBox.critical(self, 'Ошибка', f'Ошибка подключения к базе данных{error}')
+            print("Ошибка подключения к базе данных: ")
+            return None
--- gostinitsa/main_window.py
+++ gostinitsa/main_window.py
@@ -1,18 +1,41 @@
 import sys, psycopg2
-from PyQt6.QtCore import QEvent, Qt
+import csv
+from PyQt6.QtCore import Qt, QEvent, pyqtSignal
 from PyQt6.QtGui import QAction
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QMessageBox, QGridLayout, QLabel, QLineEdit, \
-    QHBoxLayout, QVBoxLayout, QWidget
+from PyQt6.QtWidgets import QApplication, QTableWidget,QMainWindow, QPushButton, \
+    QMessageBox, QGridLayout, QLabel, QLineEdit, QHBoxLayout, QVBoxLayout, QWidget, QFileDialog
 
 from widget_add_client import add_client_window
-
-DATABASE = "Gostinitsa"
-HOST = "localhost"
-PORT = "5432"
+from login_window import LogInWindow
 
 class MainWindow(QMainWindow):
-    def __init__(self):
+    def __init__(self, connection):
         super().__init__()
+        query = "SELECT table_name \
+                FROM information_schema.tables \
+                WHERE table_schema = 'public';"
+
+        self.connection = connection
+        central_widget = QWidget()
+
+        btn_check = QPushButton('Проверить')
+        #btn_check.clicked.connect(lambda: self.get_table_names(query))
+        btn_check.clicked.connect(self.display_all_fields)
+
+        btn_add_all = QPushButton('Добавить всех клиентов')
+        #btn_add_all.clicked.connect(self.open_csv_and_add_clients)
+
+        btn_remove_all = QPushButton('Удалить всех клиентов')
+        #btn_add_all.clicked.connect(self.remove_all_clients)
+
+
+        test_layout = QVBoxLayout()
+        test_layout.addWidget(btn_check, alignment=Qt.AlignmentFlag.AlignCenter)
+        test_layout.addWidget(btn_add_all, alignment=Qt.AlignmentFlag.AlignCenter)
+        test_layout.addWidget(btn_remove_all, alignment=Qt.AlignmentFlag.AlignCenter)
+        central_widget.setLayout(test_layout)
+        self.setCentralWidget(central_widget)
+
         '''Работа с меню'''
         self.setWindowTitle('гостиница')
 
@@ -37,104 +60,60 @@ class MainWindow(QMainWindow):
 
         help_menu.addAction(about_action)
 
-
-        '''Работа с главным окном'''
-        #self.connect = self.connect_to_database()
-
-        central_widget = QWidget(self)
-        self.setCentralWidget(central_widget)
-
-        db_name_label = QLabel(f'Подключение к базе данных {DATABASE}')
-
-        btn_connect = QPushButton('Подключиться')
-        btn_connect.setAutoDefault(True)
-
-        user_label = QLabel()
-        user_label.setText('Пользователь: ')
-
-        password_label = QLabel()
-        password_label.setText('Пароль: ')
-
-        self.user_line = QLineEdit()
-        self.user_line.returnPressed.connect(btn_connect.click)
-        self.password_line = QLineEdit()
-        self.password_line.returnPressed.connect(btn_connect.click)
-        self.password_line.setEchoMode(QLineEdit.EchoMode.Password)
-
-        form_layout = QGridLayout()
-        form_layout.addWidget(user_label, 0, 0)
-        form_layout.setAlignment(user_label, Qt.AlignmentFlag.AlignRight)
-
-        form_layout.addWidget(self.user_line, 0, 1)
-        form_layout.setAlignment(self.user_line, Qt.AlignmentFlag.AlignLeft)
-
-        form_layout.addWidget(password_label, 1, 0)
-        form_layout.setAlignment(password_label, Qt.AlignmentFlag.AlignRight)
-
-        form_layout.addWidget(self.password_line, 1, 1)
-        form_layout.setAlignment(self.password_line, Qt.AlignmentFlag.AlignLeft)
-
-        main_layout = QVBoxLayout()
-        main_layout.addWidget(db_name_label, alignment=Qt.AlignmentFlag.AlignCenter)
-        main_layout.addLayout(form_layout)
-        main_layout.addWidget(btn_connect, alignment=Qt.AlignmentFlag.AlignCenter)
-
-        central_widget.setLayout(main_layout)
-
-        self.setFixedSize(300, 200)
-
-        btn_connect.clicked.connect(self.connect_to_database)
+        self.setFixedSize(600, 600)
     def open_add_client_window(self):
         self.client_window = add_client_window()
         self.client_window.show()
 
-    def connect_to_database(self):
-        user = self.user_line.text()
-        password = self.password_line.text()
-
-        if not all([user, password]):
-            self.blank_error_box()
-            return None
+    def execute_query(self, query, params=None):
+        """Выполняет SQL-запрос с использованием переданного соединения."""
         try:
-            connection = psycopg2.connect(
-                database=DATABASE,
-                user=user,
-                password=password,
-                host=HOST,
-                port=PORT
-            )
-            self.success_box  = QMessageBox(self)
-            self.success_box.setWindowTitle('Успех')
-            self.success_box.setStandardButtons(QMessageBox.StandardButton.Ok)
-            self.success_box.setText(f'Успешное подключение к базе данных {DATABASE}')
-            self.success_box.exec()
-            print("Успешное подключение к базе данных!")
-            return connection
-        except (Exception, psycopg2.Error) as error:
-            self.error_box = QMessageBox(self)
-            self.error_box.setIcon(QMessageBox.Icon.Warning)
-            self.error_box.setWindowTitle('Ошибка')
-            self.error_box.setStandardButtons(QMessageBox.StandardButton.Ok)
-            self.error_box.setText(f"Ошибка подключения к базе данных: {error}")
-            self.error_box.exec()
-            print("Ошибка подключения к базе данных: ", error)
+            with self.connection.cursor() as cursor:
+                cursor.execute(query, params)
+                self.connection.commit()
+                return cursor.fetchall()
+        except Exception as error:
+            QMessageBox.critical(self, 'Ошибка', f'Ошибка выполнения запроса: {error}')
             return None
 
-    def blank_error_box(self):
-        blank_error = QMessageBox(self)
-        blank_error.setText("Заполните все поля!")
-        blank_error.setStandardButtons(QMessageBox.StandardButton.Ok)
-        blank_error.exec()
+    def get_table_names(self, query, params=None):
+        results = self.execute_query(query, params)
+        if results is not None:
+            for table in results:
+                table_name = table[0]
+                print(table_name)
+    def display_all_fields(self):
+        """Получает и отображает все поля из всех таблиц в базе данных."""
+        try:
+            # Получаем список всех таблиц
+            query_tables = "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';"
+            tables = self.execute_query(query_tables)
 
-    def closeEvent(self, event):
-        if self.connection:
-            self.connection.close()
-            print("Соединение с базой данных закрыто")
-            event.accept()
+            if tables:
+                for table in tables:
+                    table_name = table[0]  # Имя таблицы
+                    print(table_name)
+
+                    # Получаем столбцы для каждой таблицы
+                    '''query_columns = "SELECT column_name FROM information_schema.columns WHERE table_name = %s;"
+                    columns = self.execute_query(query_columns, (table_name,))
+
+                    if columns:
+                        column_names = [col[0] for col in columns]  # Извлекаем имена столбцов
+                        print("Columns:", column_names)
+                    else:
+                        print("No columns found.")'''
+            else:
+                print("No tables found.")
+        except Exception as error:
+            QMessageBox.critical(self, 'Ошибка', f'Ошибка при получении данных: {error}')
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    login_window = LogInWindow()
 
-    window = MainWindow()
-    window.show()
+    login_window.connection_successful.connect(lambda conn: setattr(login_window, 'main_window',
+                                            MainWindow(conn)) or login_window.main_window.show())
+    login_window.show()
 
     sys.exit(app.exec())
